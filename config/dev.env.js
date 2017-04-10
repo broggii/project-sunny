d771a1ccb5e5c4969e9bd06dc3a803e411be6e9a
Cameron Walworth <cameron.walworth@gmail.com>
2017-04-10 13:11:49 +0900

    added firebase

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,11 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_KEY: "AIzaSyD8qYGyC2l9le4vpRZ6S8cJ7Fo6vCPkpOA",
+  AUTH_DOMAIN: "sunny-29d91.firebaseapp.com",
+  DATABASE_URL: "https://sunny-29d91.firebaseio.com",
+  PROJECT_ID: "sunny-29d91",
+  STORAGE_BUCKET: "sunny-29d91.appspot.com",
+  MESSAGING_SENDER_ID: "247618304038"
 })
